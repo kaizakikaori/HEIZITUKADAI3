e09f1ff7091e7ed1a06a048db8a3861e0932a01b
VERTEX21 <vertex21@VERTEX21noMacBook-Air.local>
2019-08-16 00:16:03 +0900

    変数名を修正

--- 平日課題3/NextViewController.swift
+++ 平日課題3/NextViewController.swift
@@ -14,31 +14,29 @@ class NextViewController: UIViewController {
     @IBOutlet weak var showName: UITextView!
     // 趣味を表示するテキストビュー
     @IBOutlet weak var showHobby: UITextView!
-
-    
+    // 名前と趣味を受け取る変数を定義
     var receiveName: String = ""
     var receiveHobby: String = ""
-
+    
     // 画面が読み込まれたときの処理
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 名前と趣味を各ラベルに表示する
         showName.text = receiveName
         showHobby.text = receiveHobby
         
-        
-      
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
